ddae7f1f4e8e29b46954bca1938513e6df5cef20
Spirkin S <s.spirkin@gmail.com>
2021-04-10 16:20:30 +0300

    Add header and theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MainPage from './routes/MainPage';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
+import { Header } from './components/Header';
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
+
+const theme = createMuiTheme();
 
 function App() {
     return (
-        <div className="App">
-            <CssBaseline />
+        <ThemeProvider theme={theme}>
+            <div>
+                <CssBaseline />
 
-            <header>
+                <Header />
 
-            </header>
-
-            <Router>
-                <Container maxWidth="lg">
-                    {/* A <Switch> looks through its children <Route>s and
-                    renders the first one that matches the current URL. */}
-                    <Switch>
-                        <Route path="/foo">
-                            <p>Foo</p>
-                        </Route>
-                        <Route path="/">
-                            <MainPage />
-                        </Route>
-                    </Switch>
-                </Container>
-            </Router>
-        </div>
+                <Router>
+                    <Container maxWidth="lg">
+                        {/* A <Switch> looks through its children <Route>s and
+                        renders the first one that matches the current URL. */}
+                        <Switch>
+                            <Route path="/foo">
+                                <p>Foo</p>
+                            </Route>
+                            <Route path="/">
+                                <MainPage />
+                            </Route>
+                        </Switch>
+                    </Container>
+                </Router>
+            </div>
+        </ThemeProvider>
     );
 }
 
