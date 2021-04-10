79095615296c8a9727953b3e4cd0df80500824bd
Spirkin S <s.spirkin@gmail.com>
2021-04-10 15:59:00 +0300

    Init commet

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import MainPage from './routes/MainPage';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
+function App() {
+    return (
+        <div className="App">
+            <CssBaseline />
+
+            <header>
+            </header>
+
+            <Router>
+                {/*<nav>*/}
+                {/*  <ul>*/}
+                {/*    <li>*/}
+                {/*      <Link to="/">Home</Link>*/}
+                {/*    </li>*/}
+                {/*    <li>*/}
+                {/*      <Link to="/foo">Foo</Link>*/}
+                {/*    </li>*/}
+                {/*  </ul>*/}
+                {/*</nav>*/}
+
+                <Container maxWidth="lg">
+                    {/* A <Switch> looks through its children <Route>s and
+                    renders the first one that matches the current URL. */}
+                    <Switch>
+                        <Route path="/foo">
+                            <p>Foo</p>
+                        </Route>
+                        <Route path="/">
+                            <MainPage />
+                        </Route>
+                    </Switch>
+                </Container>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/routes/MainPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import makeStyles from '@material-ui/styles/makeStyles';
+import
+
+const useStyles = makeStyles({
+    root: {
+
+    },
+});
+
+function MainPage() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Typography>Привет, дорогой друг! Спасибо, что согласился принять участие в нашем исследовании. Тебе будет предложено найти постер одного очень известного мультфильма: Губка Боб. Нажми если готов!</Typography>
+
+        </div>
+    );
+}
+
+export default MainPage;
